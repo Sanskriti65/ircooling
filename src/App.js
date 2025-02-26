9b22362344596cd6b93a6d0566dbfe9f485446e3
Sanskriti Shukla <138789759+Sanskriti65@users.noreply.github.com>
2025-02-26 20:04:40 +0530

    26fev

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import 'font-awesome/css/font-awesome.min.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+import HomePage from './pages/HomePage';
+import Navbar from './components/Navbar';
+import AboutPage from './pages/AboutPage';
+// import ServicesPage from './pages/ServicesPage';
+import Whychoose from './pages/Whychoose';
+import Contact from './pages/Contact';
+// import Services from './pages/Services';
+import Footer from './components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        {/* <Route path="/services" element={<ServicesPage />} /> */}
+        <Route path="/whychoose" element={<Whychoose />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
-}
+};
 
 export default App;
